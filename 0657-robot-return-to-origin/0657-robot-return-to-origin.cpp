c4c19b54aac16ca20273e2603d1ba33ab7b8da52
Vaibhav Tyagi <tyagivaibhav430@gmail.com>
2026-08-28 22:39:29 +0530

    Time: 0 ms (100%), Space: 10.3 MB (46.72%) - LeetHub

---
+++ 0657-robot-return-to-origin/0657-robot-return-to-origin.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    bool judgeCircle(string moves) {
+        int x = 0, y = 0;
+
+        for (char move : moves) {
+            if (move == 'R') x++;
+            else if (move == 'L') x--;
+            else if (move == 'U') y++;
+            else if (move == 'D') y--;
+        }
+
+        return x == 0 && y == 0;
+    }
+};
